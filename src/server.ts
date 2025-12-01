6b9ca042fd62201fa963422d881adb7a12276206
sajjad244 <mrsajjed@gmail.com>
2025-12-01 12:28:50 +0600

    update and delete

--- src/server.ts
+++ src/server.ts
@@ -90,7 +90,7 @@ app.get("/users", async (req: Request, res: Response) => {
   }
 });
 
-// get single users
+// get single user
 app.get("/users/:id", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [
@@ -107,6 +107,65 @@ app.get("/users/:id", async (req: Request, res: Response) => {
         success: true,
         message: "User fetched successfully",
         data: result.rows[0],
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err,
+    });
+  }
+});
+
+// update user
+app.put("/users/:id", async (req: Request, res: Response) => {
+  const {name, email} = req.body;
+  try {
+    const result = await pool.query(
+      `UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`,
+      [name, email, req.params.id]
+    );
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User updated successfully",
+        data: result.rows[0],
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err,
+    });
+  }
+});
+
+// delete user
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+  try {
+    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User fetched successfully",
+        data: null,
       });
     }
   } catch (err: any) {
